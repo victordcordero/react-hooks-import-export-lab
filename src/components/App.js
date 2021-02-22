272c1be2b8a38135ac4bf5b4aaaf903c841ced06
github username <github email address>
2021-02-22 16:39:02 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import NavBar from "./NavBar"
+import Home from "./Home"
+import About from "./About"
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
-      <About />
+      <About  />
     </div>
   );
 }
